d4938ef10abe6528f552ac38a31d2bd27a4ede81
Aamreet <amreetsingh508@gmail.com>
2024-05-27 00:22:14 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log("...connecting to db");
+const connectDB = async () => {
+  try {
+    console.log(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+export default connectDB;
