eb143746cc328a92e1eb1a99a1ae82b899644868
Aamreet <amreetsingh508@gmail.com>
2024-05-27 12:32:25 +0530

    updated variables

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart /> Cart
-                  {cartItems && (
+                  {cartItems.length > 0 && (
                     <Badge pill bg='success' style={{ marginLeft: '5px' }}>
                       {cartItems.reduce((a, c) => a + c.qty, 0)}
                     </Badge>
