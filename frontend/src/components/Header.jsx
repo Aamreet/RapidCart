03337c67e1d6b57a810d2ddccaeffee174502b7a
Aamreet <amreetsingh508@gmail.com>
2024-05-28 20:43:45 +0530

    updated ui components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
           <LinkContainer to='/'>
             <Navbar.Brand>
               <img src={logo} alt='ProShop' />
-              Rapid Retail
+              Rapid Cart
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
